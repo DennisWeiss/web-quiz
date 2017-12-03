3e7c6c5eb06b2b3ef5b1afcc1c6bf2235533dacd
DennisWeiss <weiss.dennis98@yahoo.de>
2017-12-03 14:38:00 +0100

    Add basic login and logout functionality

--- index.php
+++ index.php
@@ -1,5 +1,47 @@
 <?php
+const db_url = "localhost";
+const db_username = "root";
+const db_password = "Meycrosoft1337";
+const db_name = "quiz";
 
+session_start();
+
+if (isset($_SESSION['user'])) {
+    $username = $_SESSION['user'];
+    echo "Hello $username, you are logged in!<br/>";
+    echo "<form action='logout.php' method='post'><br/>";
+    echo "<input type='submit' value='Logout'><br/>";
+    echo "<input type='hidden' name='logout'>";
+} else {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $connection = new mysqli(db_url, db_username, db_password, db_name);
+        $username = $_POST['username'];
+        if ($result = $connection->query("SELECT * FROM user WHERE username = '$username';")) {
+            $row = $result->fetch_row();
+            if ($row[2] == $_POST['password']) {
+                if ($row[4] == 1) {
+                    $_SESSION['user'] = $username;
+                    header('Location: index.php');
+                } else {
+                    echo "User is not yet authenticated!";
+                }
+            } else {
+                echo "Wrong username or wrong password!";
+            }
+        } else {
+            echo "Oops, something went wrong";
+        }
+    } else {
+        echo "<form action='index.php' method='post'>";
+        echo "<input type='text' name='username' placeholder='Username or Email'><br/>";
+        echo "<input type='password' name='password' placeholder='Password'><br/>";
+        echo "<input type='submit' value='Login'>";
+        echo "</form>";
+        echo "<a href='pw-reset.php'>Forgot your password?</a><br/>";
+        echo "<a href='register.php'>Register</a>";
+
+    }
+}
 ?>
 
 <html>
@@ -7,12 +49,6 @@
     <title>Web Quiz</title>
 </head>
 <body>
-    <form action="login.php" method="post">
-        <input type="text" name="user" placeholder="Username or Email"><br/>
-        <input type="password" name="password" placeholder="Password"><br/>
-        <input type="submit" value="Login">
-    </form>
-    <a href="pw-reset.php">Forgot your password?</a><br/>
-    <a href="register.php">Register</a>
+
 </body>
 </html>
