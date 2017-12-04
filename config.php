a684b664c66e5b1946836a26b25798294d3080fe
DennisWeiss <weiss.dennis98@yahoo.de>
2017-12-04 20:44:35 +0100

    Implement email sending functionality

--- config.php
+++ config.php
@@ -6,4 +6,9 @@ $db_username = $config['db_username'];
 $db_password = $config['db_password'];
 $db_name = $config['db_name'];
 
+$email_host = $config['email_host'];
+$email_port = $config['email_port'];
+$email_address = $config['email_address'];
+$email_password = $config['email_password'];
+
 ?>
--- register.php
+++ register.php
@@ -1,4 +1,7 @@
 <?php
+require "PHP-Mailer/Exception.php";
+require "PHP-Mailer/PHPMailer.php";
+require "PHP-Mailer/SMTP.php";
 
 include "config.php";
 
@@ -17,11 +20,51 @@ if (isset($_POST['register'])) {
     $password = md5($password);
     $auth_code = random_string(15);
 
-    $connection->query("INSERT INTO user(username, email, password, auth_code, authenticated) VALUES('$username', '$email', '$password', '$auth_code', 0);");
+    send_email($email, $username, $email, $password, $auth_code, $connection);
 
     $connection->close();
 
-    echo "An authentication code has been sent to your e-mail address, please confirm your e-mail!";
+}
+
+function send_email($to, $username, $email, $password, $auth_code, $connection) {
+    include "config.php";
+
+    $msg = "Hello $username,\n\nThank you for registering. Confirm your email-address by clicking on the following link:\n\nhttp://localhost/auth.php?user=$username&authcode=$auth_code";
+    $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+    $mail->isSMTP();
+    $mail->SMTPDebug = 5;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->CharSet = "UTF-8";
+
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    $mail->Host = $email_host;
+    $mail->Port = $email_port;
+    $mail->isHTML(true);
+    $mail->Username = $email_address;
+    $mail->Password = $email_password;
+    $mail->setFrom($email_address);
+    $mail->Subject = "Authentication Code";
+    $mail->Body = $msg;
+    $mail->addAddress($to);
+
+    try {
+        $mail->send();
+        $connection->query("INSERT INTO user(username, email, password, auth_code, authenticated) VALUES('$username', '$email', '$password', '$auth_code', 0);");
+        echo "An authentication code has been sent to your e-mail address, please confirm your e-mail!";
+    } catch (\PHPMailer\PHPMailer\Exception $e) {
+        echo "Failed to send email<br>";
+        echo $mail->ErrorInfo;
+    }
+
+
+
 }
 
 function random_string($length) {
