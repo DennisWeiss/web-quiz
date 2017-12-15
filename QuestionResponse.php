8bf8b73cb5055d99b413c5093859a0480936adff
DennisWeiss <weiss.dennis98@yahoo.de>
2017-12-15 18:19:36 +0100

    Add simple class for response giving a question

---
+++ QuestionResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+class QuestionResponse
+{
+    public $type;
+    public $questionnumber;
+    public $question;
+    public $a;
+    public $b;
+    public $c;
+    public $d;
+    public $correct;
+
+    /**
+     * QuestionResponse constructor.
+     * @param $questionnumber integer cummulative number of question of current quiz
+     * @param $question string questiontext
+     * @param $a string text of answer a
+     * @param $b string text of answer b
+     * @param $c string text of answer c
+     * @param $d string text of answer d
+     * @param $correct string from a-d denoting the correct answer
+     */
+    public function __construct($questionnumber, $question, $a, $b, $c, $d, $correct)
+    {
+        $this->type = "question";
+        $this->questionnumber = $questionnumber;
+        $this->question = $question;
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $this->d = $d;
+        $this->correct = $correct;
+    }
+
+
+}
--- quiz.php
+++ quiz.php
@@ -15,10 +15,27 @@ $question_json = file_get_contents("https://opentdb.com/api.php?amount=1&categor
 
 $obj = json_decode($question_json, true);
 
-echo $obj['results'][0]['question'];
+$correct_answer = rand(0, 3);
+
+echo $obj['results'][0]['question'] . "<br>";
+
+echo $correct_answer . "<br>";
+
+$answers = array();
+
+for ($i = 0; $i < $correct_answer; $i++) {
+    array_push($answers, $obj['results'][0]['incorrect_answers'][$i]);
+}
+
+array_push($answers, $obj['results'][0]['correct_answer']);
+
+for ($i = $correct_answer; $i < 3; $i++) {
+    array_push($answers, $obj['results'][0]['incorrect_answers'][$i]);
+}
+
+print_r($answers);
 
 if (isset($_POST['getquiz'])) {
-
 
 }
 
